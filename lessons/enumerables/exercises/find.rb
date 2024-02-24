240ec1b5eb4fdb95f0629a29277e30f7ad53558e
Logan Matheny <loganpaulmatheny@gmail.com>
2024-02-24 13:24:44 -0500

    Complete the warmup and find exercises

--- lessons/enumerables/exercises/find.rb
+++ lessons/enumerables/exercises/find.rb
@@ -1,11 +1,14 @@
 # 1. Using #each, write the code to find the first sister over four letters in length.
 # 2. Once you get the method below to work using #each, refactor your code to use #find. 
 
+# Using .each
 def find_first_long_named_sister
     sisters = ["Rose", "Kathleen", "Eunice", "Patricia", "Jean"]
   
     ### YOUR CODE HERE
-
+    sisters.each do |sister|
+      return sister if sister.length > 4 
+    end 
     # should return "Kathleen"
 end
 
--- lessons/enumerables/warm_up/warm_up.rb
+++ lessons/enumerables/warm_up/warm_up.rb
@@ -4,7 +4,12 @@
 kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
 
 def upcase_names(names)
- # your code here
+  uppercase_names = []
+  names.each do |name|
+    name = name.capitalize
+    uppercase_names.push(name)
+  end 
+  uppercase_names
 end 
 
 p upcase_names(kardashians)
@@ -22,7 +27,13 @@ p upcase_names(kardashians)
 pets = ["Brutus", "Lucky", "Goldie", "Pepper", "Odie"]
 
 def names_ending_in_ie(names)
- # your code here
+  ie_names = []
+  names.each do |name|
+    if name.end_with?("ie")
+      ie_names.push(name)
+    end
+  end 
+  ie_names
 end 
 
 p names_ending_in_ie(pets)
@@ -40,7 +51,11 @@ p names_ending_in_ie(pets)
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 def first_num_thats_square_is_above_100(nums)
-
+  nums.each do |num|
+    if num ** 2 > 100
+      return num 
+    end 
+  end 
 end 
 
 p first_num_thats_square_is_above_100(nums)
